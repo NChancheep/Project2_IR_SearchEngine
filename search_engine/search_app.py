a79ec542142211dea9a098fb8b7b4411cf331281
Chancheep Mahacharoensuk <chancheep.mcs@gmail.com>
2021-11-07 19:58:55 +0700

    update

--- search_engine/search_app.py
+++ search_engine/search_app.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from markupsafe import escape
 from flask import render_template
+from elasticsearch import Elasticsearch
 
-
+es = Elasticsearch()
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,4 +12,11 @@ def index():
 
 @app.route('/search')
 def search():
-    return render_template('search.html')
+    body ={
+        'size':10,
+        'query':{
+            'match_all':{}
+        }
+    }
+    res = es.search(index='products',doc_type='',body=body)
+    return render_template('search.html',result=res)
